676faf195d9a77df3eac11961bce4cd04d21a1a0
Grace Marachi <aronumarachi01@gmail.com>
2025-07-21 00:28:33 +0100

    Added a callback function

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ const myUpskillProfile = {
   name: "Grace Aronu",
   age: 23,
   message: function() {
-    () => {
-    console.log ("Hi, my name is " + this.name )
-  }   
+    console.log ("Hi, my name is " + this.name )  
   },
   hobbies: ["Crocheting", "Watching Movies", "Reading"]
 };
 
-console.log (myUpskillProfile.hobbies[1]);
+myUpskillProfile.message();
 
+console.log ("My second hobby is: " + myUpskillProfile.hobbies[1]);
+
+
+
+// Callback Function
+
+function taskProgress () {
+    console.log("Finished task!")
+}
+
+function doTask (subject, callback) {
+    console.log("Starting Upskill task 6...")
+    callback()
+}
+
+doTask("Task 6", taskProgress);
